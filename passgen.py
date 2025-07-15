8a17a3c62868daef816e58c1017342fcd23203be
Arielss <arielm.sereno.a@gmail.com>
2025-07-14 21:21:49 -0400

    nuevo registro

--- passgen.py
+++ passgen.py
@@ -1,4 +1,10 @@
-def lar():
-    {
+import string
+import random
 
-    }
+longitud = int(input("Ingrese el tamaño de la contraseña: "))
+
+caracteres = string.ascii_letters + string.digits + string.punctuation
+
+contrasena = "".join(random.choice(caracteres) for i in range(longitud))
+
+print(f"La contraseña generada es:{contrasena}")
