92288614b36380e78ca947bd86261f838b0b66b0
Arielss <arielm.sereno.a@gmail.com>
2025-07-22 18:16:53 -0400

    Texto a voz

--- 04 - textoAVoz.py
+++ 04 - textoAVoz.py
@@ -1,10 +1,36 @@
-from gtts import gTTS
 import os
+from tkinter import Tk, Text, Button, END, Label
+from gtts import gTTS
 
-with open('textoaleer.txt', 'r', encoding="utf-8") as file:
-    texto = file.read()
+def save_text():
+    text = text_area.get("1.0", END)
+    with open('04 - user_input.txt', 'w', encoding="utf-8") as file:
+        file.write(text)
 
-audio = gTTS(texto, lang="es", slow="False")
-audio.save("output.mp3")
+    status_label.config(text="Texto guardado con éxito.")
 
-os.system('start output.mp3')
+def text_to_speech():
+    text = text_area.get("1.0", END)
+    speech = gTTS(text=text, lang='es', slow=False)
+    speech.save('output_04.mp3')
+    os.system('start output_04.mp3')
+
+    status_label.config(text='Reproduciendo audio.')
+
+ventana = Tk()
+ventana.title("Texto a voz")
+ventana.configure(bg="#dbdbde")
+
+text_area = Text(ventana, height=10, width=50)
+text_area.pack()
+
+save_button = Button(ventana, text="Guardar texto", command=save_text)
+save_button.pack()
+
+play_button = Button(ventana, text="Reproducir texto", command=text_to_speech)
+play_button.pack()
+
+status_label = Label(ventana, text="", fg="#cf5630", bg="#dbdbde")
+status_label.pack()
+
+ventana.mainloop()
