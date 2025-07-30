0c8bc80221fdb3672dc6c7ed92b69e674d90ba8b
Arielss <arielm.sereno.a@gmail.com>
2025-07-30 15:20:35 -0400

    Nuevo proyecto reservas con selenium

---
+++ 12 - Recinto Puente Alto.py
@@ -0,0 +1,44 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.get("https://deportespuentealto.cl/recintos")
+time.sleep(2)
+
+#Busca el tipo de instalación
+campoBusqueda = driver.find_element(By.NAME, "macrotag_id")
+campoBusqueda.send_keys("Instalaciones deportivas")
+time.sleep(0.5)
+
+#Busca la reserva
+campoReserva = driver.find_element(By.NAME, "tag_id")
+campoReserva.send_keys("Futbolito")
+time.sleep(0.5)
+
+#Cambiar fecha a la correspondiente
+campoFecha = driver.find_element(By.NAME, "date")
+campoFecha.clear()
+campoFecha.send_keys("31/07/2025")
+time.sleep(0.5)
+
+#Hora siempre en 00:00
+campoHora = driver.find_element(By.NAME, "start_time")
+campoHora.send_keys("00:00")
+time.sleep(0.5)
+
+driver.find_element(By.CLASS_NAME, "homeSearchForm-button").click()
+time.sleep(4)
+
+driver.find_element(By.CSS_SELECTOR, "[title='COMPLEJO DOMINGO TOCORNAL']").click()
+time.sleep(4)
+
+
+
+time.sleep(10000)
+
+
+
+
