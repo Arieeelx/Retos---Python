8328992cb571ab469df05c63f42d884e932d8553
Devar <arielm.sereno.a@gmail.com>
2025-09-13 19:31:09 -0300

    Nuevo proyecto D4

---
+++ Python en 16 días/D4 - Adivina el número.py
@@ -0,0 +1,8 @@
+#Consultar por nombre y qué usuario adivine el número entre el 1-100 y posee 8 intentos para adivinarlo. Posee 4 Criterios
+
+#Si el N es < a 1 o > 100 = N° no permitido
+#si el N de usuario elige un N menor al que indica el programa, este dirá que número es mayor al elegido.
+#si el N de usuario elige un N mayor al que indica el programa, este dirá que número es menor al elegido.
+#si el usuario acierta se le informa que ha ganado y en cuantos intentos.
+
+nombreUsuario = input("Ingresa tu nombre: ")
