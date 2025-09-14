581cbbdad63c5043eeac54c2051567af7ce39b56
Devar <arielm.sereno.a@gmail.com>
2025-09-14 18:18:57 -0300

    Proyecto D4 Terminado

--- Python en 16 días/D4 - Adivina el número.py
+++ Python en 16 días/D4 - Adivina el número.py
@@ -1,33 +1,50 @@
 from random import *
 
-#Consultar por nombre y qué usuario adivine el número entre el 1-100 y posee 8 intentos para adivinarlo. Posee 4 Criterios
+nombreUsuario = input("\nBienvenidos a 'ADIVINA EL NÚMERO SECRETO' en un rango de 1-100, ¿Podrás?.\nDe premio, te ganas senda patá en la raja.\nAhora, ingresa tu nombre para conocerte: ")
 
-#Si el N es < a 1 o > 100 = N° no permitido
-#si el N de usuario elige un N menor al que indica el programa, este dirá que número es mayor al elegido.
-#si el N de usuario elige un N mayor al que indica el programa, este dirá que número es menor al elegido.
-#si el usuario acierta se le informa que ha ganado y en cuantos intentos.
 
-nombreUsuario = input("Ingresa tu nombre: ")
-respuestasUsuario = []
-numeroSecreto = randint(1,101)
+def nuevoJuego():
+    numeroSecreto = randint(1, 101)
+    respuestasUsuario = []
+    acertado = False
+    print(f"\nUn gusto {nombreUsuario}.\nCreando número...\nNúmero secreto creado, ¿Crees poder adivinarlo?")
 
-print(numeroSecreto)
+    for intento in range(8):
+        intentosRestantes = 8 - intento - 1
 
-for intento in range(8):
+        try:
+            respuestasUsuario.append(int(input("\nIngresa tu respuesta: ")))
+            if respuestasUsuario[intento] < 1 or respuestasUsuario[intento] > 100:
+                print(f"\n¡Mal ahí {nombreUsuario}! \nDebes ingresar un número entre el 1-100.\nQuedan {intentosRestantes} intentos.")
+            elif respuestasUsuario[intento] == numeroSecreto:
+                print(f"\n¡Has adivinado {nombreUsuario}! \nEl número secreto era: {numeroSecreto} y faltando {intentosRestantes} intentos.")
+                acertado = True
+                break
+            elif respuestasUsuario[intento] > numeroSecreto:
+                print(f"\nEquivocado {nombreUsuario}.\nEl número secreto es menor al elegido.\nQuedan {intentosRestantes} intentos.")
+            elif respuestasUsuario[intento] < numeroSecreto:
+                print(f"\nEquivocado {nombreUsuario}.\nEl número secreto es mayor al elegido.\nQuedan {intentosRestantes} intentos.")
+        finally:
+            pass
 
-    try:
-        respuestasUsuario.append(int(input("Ingresa tu respuesta: ")))
-        if respuestasUsuario[intento] < 1 or respuestasUsuario[intento] > 100:
-            print(f"¡Mal ahí {nombreUsuario}! \nDebes ingresar un número entre el 1-100.\nQuedan: {intento} intentos.")
-        elif respuestasUsuario[intento] == numeroSecreto:
-            print(f"¡Has adivinado {nombreUsuario}! \nEl número secreto era: {numeroSecreto} y tan solo en {intento} intentos.")
+    if not acertado:
+        print(f"Caaaaaaaaasi papu.\nEl número secreto era: {numeroSecreto}, para una próxima será...")
+
+
+def aJugar():
+    while True:
+        nuevoJuego()
+        seguirJugando = input("\n¿Deseas jugar otra vez?\ns: Seguir jugando.\nn: Quiero irme a la camita\ns/n")
+
+        if seguirJugando.lower() == "s":
+            nuevoJuego()
+        elif seguirJugando.lower() == "n":
+            print("Hasta la vista bby")
             break
-        elif respuestasUsuario[intento] > numeroSecreto:
-            print(f"Equivocado {nombreUsuario}.\nEl número secreto es menor al elegido.\nQuedan: {intento} intentos.")
-        elif respuestasUsuario[intento] < numeroSecreto:
-            print(f"Equivocado {nombreUsuario}.\nEl número secreto es mayor al elegido.\nQuedan: {intento} intentos.")
-    finally:
-        pass
+
+aJugar()
+
+
 
 
 
