d4b0744a50d0c43daa7b2c846d222cb383d5a151
Devar <arielm.sereno.a@gmail.com>
2025-09-14 15:05:12 -0300

    Se continua proyecto D4

--- Python en 16 días/D4 - Adivina el número.py
+++ Python en 16 días/D4 - Adivina el número.py
@@ -1,3 +1,5 @@
+from random import *
+
 #Consultar por nombre y qué usuario adivine el número entre el 1-100 y posee 8 intentos para adivinarlo. Posee 4 Criterios
 
 #Si el N es < a 1 o > 100 = N° no permitido
@@ -6,3 +8,11 @@
 #si el usuario acierta se le informa que ha ganado y en cuantos intentos.
 
 nombreUsuario = input("Ingresa tu nombre: ")
+
+respuestasUsuario = []
+
+for i in range(8):
+    respuestasUsuario.append(input("Ingresa tu respuesta: "))
+    if respuestasUsuario == randint(0, 101):
+        print("¡Has ganado! En tan solo ")
+
